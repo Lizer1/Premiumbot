9ab5c91f9985a2e7c9aaf5cd55c8da900e8023a0
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-25 11:50:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "NDIyMDM3NzQwNTkxMzgyNTI4.DYV8zA.6vBJjF7Op9T_bH_YOwRMXqwh9vc";
+const discord_token = "NTM4MjkzMjAyNjE3MjM3NTA1.DyxsYA.3PjuJt_eLFI-oWHQfcpFcjJbg6c";
 var table = require('table').table
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
